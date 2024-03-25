db3161019e805d8333e10c6ae5110c378631286d
jenifertorresnavarro <jenifertorresnavarro@gmail.com>
2024-03-24 20:32:02 -0700

    input the router get sections

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,37 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    // finding all categories
+    const categories = await Category.findAll({ include: [{ model: Product }] });
+    res.status(200).json(categories);
+  } catch (err) {
+    // for handling errors
+    res.status(500).json({ message: 'not found!' });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    // finding the category with the matching ID
+    const category = await Category.findByPk(req.params.id, { include: [{ model: Product }] });
+
+    // error message 
+    if (!category) {
+      res.status(404).json({ message: 'id not found' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    // handling errors
+    res.status(500).json({ message: 'not found!' });
+  }
 });
 
 router.post('/', (req, res) => {
